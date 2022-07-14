baa4d2b9c4b0fb93fcfde277c22dd433a86a39f1
eweb-ossy <ossy2000@gmail.com>
2022-07-14 16:30:37 +0900

    add: campaign detail page

---
+++ dist/campaign_detail.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex">
+    <title>キャンペーン ｜ゑびすフードファクトリー</title>
+    <link rel="stylesheet" href="css/campaign_detail.css?<?= time() ?>">
+</head>
+<body>
+    <?php require('include/header.php'); ?>
+    <div class="catch">
+        <h2>キャンペーン<span>Campaign</span></h2>
+    </div>
+    <div class="breadcrumblist">
+        <ul>
+            <li><a href="https://ebisu-gp.jp/factory/">TOP</a></li>
+            <li><a href="campaign.html">キャンペーン</a></li>
+            <li><a href="campaign_detail.html">OEM提携企業様を募集しております</a></li>
+        </ul>
+    </div>
+    <section class="campaign-detail">
+        <div class="inner">
+            <div class="date new">2022.06.06</div>
+            <div class="title">レシピ投稿キャンペーン開催中！</div>
+            <div class="image">
+                <img src="images/campaign_detail_photo.jpg" loading="lazy" alt="">
+            </div>
+            <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+            <nav class="pagination">
+                <div class="nav-links">
+                    <ul>
+                        <li><a href="" class="prev">前の記事</a></li>
+                        <li><a href="" class="next">次の記事</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <div class="btn">
+                <a href="news.html" class="factory-btn">キャンペーン一覧に戻る</a>
+            </div>
+        </div>
+    </section>
+    <?php require('include/contact.php'); ?>
+    <?php require('include/footer.php'); ?>
+    <script src="js/campaign_detail.js?<?= time() ?>"></script>
+</body>
+</html>
