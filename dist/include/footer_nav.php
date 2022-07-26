a3c8b25c11dfaf8d726ec6f1069a4322077c87a5
eweb-ossy <ossy2000@gmail.com>
2022-07-27 08:45:26 +0900

    fix

--- dist/include/footer_nav.php
+++ dist/include/footer_nav.php
@@ -5,8 +5,8 @@
             <a href="" class="logo">健康と、おいしさを、「つくる」<img src="images/logo_factory_column_color.svg" alt="EBISU FOOD FACTORY"></a>
             <div class="text">
                 <h3>ゑびすフードファクトリー</h3>
-                <p>〒987-2233 宮城県栗原市築館照越永平1-1</p>
-                <p>TEL.0228-22-3110　FAX.0228-22-3301</p>
+                <p>〒987-2252　宮城県栗原市築館薬師４丁目10-16</p>
+                <p>TEL.0228-24-7015　FAX.0228-23-2470</p>
             </div>
         </div>
         <div class="menu">
